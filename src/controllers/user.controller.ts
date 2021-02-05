51ed737e41e9bfe60e4bc1ce288a3159891fcda7
D4RKAR117 <D4RKAR117@outlook.es>
2021-02-04 22:26:15 -0500

    CRUD for user entity

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,4 +7,32 @@ const getUsers = async (req: Request, res: Response): Promise<Response> => {
 	return res.json(users);
 };
 
-export { getUsers };
+const createUser = async (req: Request, res: Response): Promise<Response> => {
+	const newUser = getRepository(User).create(req.body);
+	const result = await getRepository(User).save(newUser);
+	return res.json(result);
+};
+
+const searchUser = async (req: Request, res: Response): Promise<Response> => {
+	const user = await getRepository(User).findOne(req.params?.id);
+	if (user) return res.json(user);
+	return res.status(404).json({ msg: 'User Not Found' });
+};
+
+const updateUser = async (req: Request, res: Response): Promise<Response> => {
+	const user = await getRepository(User).findOne(req.params?.id);
+	if (user) {
+		getRepository(User).merge(user, req.body);
+		const result = await getRepository(User).save(user);
+		return res.json(result);
+	}
+
+	return res.status(404).json({ msg: 'User Not Found' });
+};
+
+const deleteUser = async (req: Request, res: Response): Promise<Response> => {
+	const result = await getRepository(User).delete(req.params?.id);
+	return res.json(result);
+};
+
+export { getUsers, createUser, searchUser, updateUser, deleteUser };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { getUsers } from '@controller/user.controller';
+import { getUsers, createUser, searchUser, updateUser, deleteUser } from '@controller/user.controller';
 
 const router = Router();
 
 router.get('/users', getUsers);
-/* router.get('/user/:id');
-router.post('/users/create');
-router.put('/user/update');
-router.delete('/user/:id'); */
+router.post('/users/create', createUser);
+router.get('/user/:id', searchUser);
+router.put('/user/:id/update', updateUser);
+router.delete('/user/:id', deleteUser);
 
 export default router;
